f153e5e3503de7e9773534e8765e23a4b38b94b4
johnnkp <johnnkp@hotmail.com>
2019-03-30 21:41:29 +0800

    Add Azure Face-related project code

--- app/src/main/java/hkcc/ccn3140/azureface/MainActivity.java
+++ app/src/main/java/hkcc/ccn3140/azureface/MainActivity.java
@@ -11,6 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+
 import java.io.IOException;
 /* import java.io.*;
 import android.app.*;
@@ -20,11 +23,23 @@ import android.os.*;
 import android.view.*;
 import android.graphics.*;
 import android.widget.*;
-import android.provider.*; */
+import android.provider.*;
+import com.microsoft.projectoxford.face.*;
+import com.microsoft.projectoxford.face.contract.*; */
 
 public class MainActivity extends AppCompatActivity {
     private final int PICK_IMAGE = 1;
     private ProgressDialog detectionProgressDialog;
+
+    // Replace `<API endpoint>` with the Azure region associated with
+    // your subscription key. For example,
+    // apiEndpoint = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0"
+    private final String apiEndpoint = "<API endpoint>";
+    // Replace `<Subscription Key>` with your subscription key.
+    // For example, subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+    private final String subscriptionKey = "<Subscription Key>";
+    private final FaceServiceClient faceServiceClient =
+            new FaceServiceRestClient(apiEndpoint, subscriptionKey);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
