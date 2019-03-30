8f731fff19c39423cd5e46c18c3de55862e8f1f5
johnnkp <johnnkp@hotmail.com>
2019-03-30 21:10:33 +0800

    Initial commit

---
+++ app/src/main/java/hkcc/ccn3140/azureface/MainActivity.java
@@ -0,0 +1,66 @@
+package hkcc.ccn3140.azureface;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+/* import java.io.*;
+import android.app.*;
+import android.content.*;
+import android.net.*;
+import android.os.*;
+import android.view.*;
+import android.graphics.*;
+import android.widget.*;
+import android.provider.*; */
+
+public class MainActivity extends AppCompatActivity {
+    private final int PICK_IMAGE = 1;
+    private ProgressDialog detectionProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(
+                        intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+        detectionProgressDialog = new ProgressDialog(this);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK &&
+                data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), uri);
+                ImageView imageView = findViewById(R.id.imageView1);
+                imageView.setImageBitmap(bitmap);
+
+                // Comment out for tutorial
+                // detectAndFrame(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
